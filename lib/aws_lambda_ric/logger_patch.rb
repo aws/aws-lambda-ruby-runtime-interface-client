0e6ff7a88ebbcfb8921951760d00c36ef1a567d1
Piotr Wasiak <chooosen@gmail.com>
2024-06-28 14:03:30 +0200

    Simpified changes with manipulating only required params

--- lib/aws_lambda_ric/logger_patch.rb
+++ lib/aws_lambda_ric/logger_patch.rb
@@ -4,19 +4,14 @@ module LoggerPatch
   def initialize(logdev, shift_age = 0, shift_size = 1048576, level: 'debug',
                  progname: nil, formatter: nil, datetime_format: nil,
                  binmode: false, shift_period_suffix: '%Y%m%d')
-    #  use unpatched constructor if logdev is a filename or an IO Object other than $stdout or $stderr
-    if logdev && logdev != $stdout && logdev != $stderr
-      super(logdev, shift_age, shift_size, level: level, progname: progname,
-                      formatter: formatter, datetime_format: datetime_format,
-                      binmode: binmode, shift_period_suffix: shift_period_suffix)
-    else
-      self.level = level
-      self.progname = progname
+    logdev_lambda_overwrite = logdev
+    if !logdev || logdev == $stdout || logdev == $stderr
+      logdev_lambda_overwrite = AwsLambdaRuntimeInterfaceClient::TelemetryLoggingHelper.telemetry_log_sink
       @default_formatter = LambdaLogFormatter.new
-      self.datetime_format = datetime_format
-      self.formatter = formatter
-      @logdev = AwsLambdaRuntimeInterfaceClient::TelemetryLoggingHelper.telemetry_log_sink
-      @level_override = {}
     end
+
+    super(logdev_lambda_overwrite, shift_age, shift_size, level: level, progname: progname,
+                                    formatter: formatter, datetime_format: datetime_format,
+                                    binmode: binmode, shift_period_suffix: shift_period_suffix)
   end
 end
