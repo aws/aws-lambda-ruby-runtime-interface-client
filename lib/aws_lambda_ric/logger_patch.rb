d9cb0a0bbe4fe0ed35189b6ff11947cb148f32ca
Mohammed Ehab Elsaeed <33024315+M-Elsaeed@users.noreply.github.com>
2025-06-05 12:41:56 +0300

    Fix Formatting override being ignored. (#47)
    * Fix Formatter Override Being Ignored.
    
    ---------
    
    Co-authored-by: Mohammed Ehab <moehabe@amazon.com>

--- lib/aws_lambda_ric/logger_patch.rb
+++ lib/aws_lambda_ric/logger_patch.rb
@@ -4,15 +4,16 @@ module LoggerPatch
   def initialize(logdev, shift_age = 0, shift_size = 1048576, level: 'debug',
                  progname: nil, formatter: nil, datetime_format: nil,
                  binmode: false, shift_period_suffix: '%Y%m%d')
-    logdev_lambda_overwrite = logdev
+    logdev_lambda_override = logdev
+    formatter_override = formatter
     # use unpatched constructor if logdev is a filename or an IO Object other than $stdout or $stderr
     if !logdev || logdev == $stdout || logdev == $stderr
-      logdev_lambda_overwrite = AwsLambdaRIC::TelemetryLogger.telemetry_log_sink
-      @default_formatter = LogFormatter.new
+      logdev_lambda_override = AwsLambdaRIC::TelemetryLogger.telemetry_log_sink
+      formatter_override = LogFormatter.new
     end
 
-    super(logdev_lambda_overwrite, shift_age, shift_size, level: level, progname: progname,
-                                    formatter: formatter, datetime_format: datetime_format,
+    super(logdev_lambda_override, shift_age, shift_size, level: level, progname: progname,
+                                    formatter: formatter_override, datetime_format: datetime_format,
                                     binmode: binmode, shift_period_suffix: shift_period_suffix)
   end
 end
